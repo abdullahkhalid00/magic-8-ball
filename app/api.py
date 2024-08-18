f8cef5eda464caa077c0391d0a5805e5f5015db6
abdullahkhalid00 <iabdulahk@gmail.com>
2024-08-18 22:33:07 +0500

    Structure repository for production

--- app/api.py
+++ app/api.py
@@ -1,5 +1,3 @@
-import uvicorn
-
 from inference import *
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
@@ -20,11 +18,7 @@ async def ask_question(request: Request):
     try:
         data = await request.json()
         question = data.get('question', '')
-        response = generate_response(question, instructions=load_instructions('./instructions.txt'))
+        response = generate_response(question, instructions=load_instructions('../instructions.txt'))
         return {'response': response.strip()}
     except Exception as e:
         return {'error': str(e)}
-
-
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
