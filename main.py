dad56710b6b8887a3b219a1e40962ccec04cc984
abdullahkhalid00 <iabdulahk@gmail.com>
2024-08-18 15:21:57 +0500

    Add CLI support

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ if __name__ == "__main__":
         q = input("What's your question? ")
         print('Thinking...')
         time.sleep(3)
+        """
+        responses = get_response_list('./responses.txt')
+        response = responses[random.randint(0, len(responses) - 1)]
+        """
         response = generate_response(question=q, instructions=load_instructions('./instructions.txt'))
         print(f"The 8 ball says: {response.strip()}")
         flag = input('Would you like to ask again? (Y/n) ')
